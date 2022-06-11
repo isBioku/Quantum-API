e2fe8dce2865f20dee3f1018931f7cab5b89ca1e
BIOKU <bioku4real@gmail.com>
2022-06-11 06:11:05 +0100

    Added placeholder to the form

--- oqc_test/input_form.py
+++ oqc_test/input_form.py
@@ -4,5 +4,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 
 class InputForm(FlaskForm):
-    inputString = StringField('Input String', [InputRequired()])
+    inputString = StringField('Input String', [InputRequired()], render_kw={"placeholder": "Please fill in a Job"})
     submit = SubmitField('Execute')
